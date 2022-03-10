51a73cd3f3b4316d963c13a28a9bd422c4ee9b30
nkg29 <noragera@microsoft.com>
2022-03-10 09:12:46 -0800

    Added cards for links to the favicon and how to actually build a website on Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-website';
+  howTo = 'https://www.youtube.com/watch?v=LEFzPe9LTzQ';
+  iconCred = 'https://th.bing.com/th/id/R.b1087919b1cb15bb754000418bd37c16?rik=q%2bZYIraEQi1J7Q&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2f4c9%2faBK%2f4c9aBKnoi.png&ehk=H8gtcUqmZsbpmvy50ZAa236ZKgWpQLb7VQj7BP7Q6GY%3d&risl=&pid=ImgRaw&r=0'
 }
